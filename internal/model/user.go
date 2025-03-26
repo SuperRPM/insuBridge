288ba20c3b41c28bddbca7e40c3031609f037da3
sylee <vidafirmar@gmail.com>
2025-03-26 14:56:51 +0900

    refactor: phone 컬럼 VARCHAR(20)으로 변경 및 유니크 인덱스 추가

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,9 +1,35 @@
 package model
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name           string `json:"name" gorm:"not null"`
+	Phone          string `json:"phone" gorm:"not null"`
+	Location       string `json:"location"`
+	PrefferedTime  string `json:"preffered_time"`
+	MonthlyPremium int    `json:"monthly_premium"`
+}
+
 type UserRequest struct {
 	Name           string `json:"name" binding:"required"`
 	Phone          string `json:"phone" binding:"required"`
 	Location       string `json:"location"`
 	PrefferedTime  string `json:"preffered_time"`
 	MonthlyPremium int    `json:"monthly_premium"`
+}
+
+type UserResponse struct {
+	ID             uint      `json:"id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	Name           string    `json:"name"`
+	Phone          string    `json:"phone"`
+	Location       string    `json:"location"`
+	PrefferedTime  string    `json:"preffered_time"`
+	MonthlyPremium int       `json:"monthly_premium"`
 }
